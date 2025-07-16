9db66c0f828e6a91d8f19e7465b15d81a654b883
HPrangsuda <k.prangsuda@hotmail.com>
2025-07-16 08:31:19 +0700

    update login

--- src/app/module/auth/signin/signin.component.ts
+++ src/app/module/auth/signin/signin.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService, LoginResponse } from '../../../services/auth.service';
 import { NotifyService } from '../../../core/service/notify.service';
@@ -8,9 +8,10 @@ import { NotifyService } from '../../../core/service/notify.service';
   standalone: false,
   templateUrl: './signin.component.html'
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit {
   username: string = '';
   password: string = '';
+  rememberMe: boolean = false;
 
   constructor(
     private router: Router,
@@ -18,7 +19,27 @@ export class SigninComponent {
     private notifyService: NotifyService
   ) {}
 
+  ngOnInit(): void {
+    // Load saved credentials if they exist
+    const savedUsername = localStorage.getItem('rememberedUsername');
+    const savedPassword = localStorage.getItem('rememberedPassword');
+    if (savedUsername && savedPassword) {
+      this.username = savedUsername;
+      this.password = savedPassword;
+      this.rememberMe = true;
+    }
+  }
+
   onSignIn(): void {
+    // Save credentials if rememberMe is checked
+    if (this.rememberMe) {
+      localStorage.setItem('rememberedUsername', this.username);
+      localStorage.setItem('rememberedPassword', this.password);
+    } else {
+      localStorage.removeItem('rememberedUsername');
+      localStorage.removeItem('rememberedPassword');
+    }
+
     this.authService.login(this.username, this.password).subscribe({
       next: (response: any) => {
         const loginData = response.body as LoginResponse;
